efbf30f929f91f033fcf53888ccc6dee9f4721ef
joanmarmu <jose-antonio.martinez@soprasteria.com>
2018-03-06 17:34:12 +0100

    Step #5: Here you will replace the hardcoded HTML with dynamic markup using Angular directives (eg ng-repeat) and {{ }} interpolation markup to utilize the User and App functionality we just imported in Step #4.
    Add the ng-controller tag to the <body> element
    Use dynamic HTML that will be compiled and rendered by Angular
    Register a custom icon set of 'user' avatars for the user list
    Register menu and share icon urls for the md-buttons

--- app/src/app.js
+++ app/src/app.js
@@ -8,10 +8,14 @@ import 'angular-material';
 import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
-export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
-  .run(() => {
-    //...
-  });
+export default angular.module( "starter-app", [ 'ngMaterial', Users.name ] )
+.config(($mdIconProvider) => {
+ // Register the user `avatar` icons
+ $mdIconProvider
+   .defaultIconSet("./assets/svg/avatars.svg", 128)
+   .icon("menu", "./assets/svg/menu.svg", 24)
+   .icon("share", "./assets/svg/share.svg", 24);
+  })
   .controller('AppController', AppController);
 /*
 export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
